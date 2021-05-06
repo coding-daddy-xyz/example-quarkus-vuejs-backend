b012e8ddf47267c048e7cc845ac166a87c0eb1f4
serhiy <serhiy.boychenko@gmail.com>
2021-05-06 12:10:25 +0200

    Refactor Main class

--- src/main/java/xyz/codingdaddy/Main.java
+++ src/main/java/xyz/codingdaddy/Main.java
@@ -8,31 +8,29 @@ import xyz.codingdaddy.domain.User;
 import javax.transaction.Transactional;
 
 @QuarkusMain
-public class Main {
+public class Main implements QuarkusApplication {
+
     public static void main(String... args) {
-        Quarkus.run(MyApp.class, args);
+        Quarkus.run(Main.class, args);
     }
 
-    public static class MyApp implements QuarkusApplication {
+    @Override
+    public int run(String... args) {
+        addTestUser("user");
+        addTestUser("admin");
 
-        @Override
-        public int run(String... args) {
-            addTestUser("user");
-            addTestUser("admin");
+        Quarkus.waitForExit();
 
-            Quarkus.waitForExit();
+        return 0;
+    }
 
-            return 0;
-        }
+    @Transactional
+    public void addTestUser(String username) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(username);
+        user.setEmail(username.concat("@example.org"));
 
-        @Transactional
-        public void addTestUser(String username) {
-            User user = new User();
-            user.setUsername(username);
-            user.setPassword(username);
-            user.setEmail(username.concat("@example.org"));
-
-            user.persistAndFlush();
-        }
+        user.persistAndFlush();
     }
 }
